8f0fd2911682e899e70eb21e8f697b5b7d946519
yefulin <yefulincode@gmail.com>
2017-12-24 14:54:58 +0800

    增加首页规格

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #coding: utf-8
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8050, debug=True)
