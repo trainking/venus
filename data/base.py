657b1bbab2fd9b5d1ed5c7829ce49cd67566915b
yefulin <yefulincode@gmail.com>
2018-01-14 21:40:06 +0800

    增加获取基本信息的方法

--- data/base.py
+++ data/base.py
@@ -10,8 +10,20 @@ def k_chart_data(code):
         Returns:
             [] 股票收盘价数据
     """
-    data = ts.get_hist_data(code)
-    r = data.close
+    _data = ts.get_hist_data(code)
+    r = _data.close
     return [
             (x,r[x]) for x in r.index
         ]
+
+def info(code):
+    """
+        根据code获取基本信息
+        Args:
+            code：股票代码
+        Returns:
+            {}
+    """
+    _data = ts.get_stock_basics()
+    _item = dict(_data.loc[code])
+    return _item
