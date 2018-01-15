34c6291b5c90c3faea913b3024f77ed9f89d5e9b
yefulin <yfl@szxlzkj.com>
2018-01-15 15:29:20 +0800

    增加获取股票收盘价的边界函数

--- data/base.py
+++ data/base.py
@@ -27,3 +27,19 @@ def info(code):
     _data = ts.get_stock_basics()
     _item = dict(_data.loc[code])
     return _item
+
+def border(code, day=None):
+    """
+       根据code价格区间，最大值以及最小值
+        Args:
+            code：股票代码
+        Returns:
+            () 第一个参数最小值，第二个参数最大值
+    """
+    _d = ts.get_hist_data(code)
+    _c = None
+    if day is not None:
+        _c = _d.close[-day:]
+    else:
+        _c = _d.close
+    return (round(_c.min(), 2), round(_c.max(), 2))
